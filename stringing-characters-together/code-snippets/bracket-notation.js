0bc9709ed226d3e4953526a4e63d90d8e6b13338
Luis Avecilla <avecillalu92@gmail.com>
2024-01-21 17:58:04 -0500

    done studio

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -2,3 +2,7 @@ let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
+console.log(phrase.length);
--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,8 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "cat";
+let name = "Garen";
+let verb = "Sing";
+let adjective = "Nicer";
+let color = "Blue";
 
 console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+console.log(`JavaScript provides a ${color} collection of tools - including ${adjective} syntax`)
