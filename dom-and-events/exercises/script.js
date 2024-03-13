5f3db4ca261b53d7955f50e16101e0754a1e14bc
Luis Avecilla <avecillalu92@gmail.com>
2024-03-12 22:30:52 -0400

    exercises done

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,21 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
-}
+   button.addEventListener('click', event => {
+    paragraph.innerHTML = 'Huston! We have liftoff!'
+   });
 
+
+//let missionAbort = document.getElementById("abortMission")
+missionAbort.addEventListener('click', event =>{
+   let abort = window.confirm("Are you sure you want to abort the mission?")
+   if(abort){
+   // let statusReport = document.getElementById("statusReport");
+    paragraph.textContent= "Mission aborted! Space shuttle returning home";
+   }
+})
+}
+/*
+
+ */
 window.addEventListener("load", init);
